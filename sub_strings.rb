17d5215841b2640846957362db8e78987a0e47d0
Guillermo <guillermoc93@protonmail.com>
2021-02-28 13:52:34 -0600

    Finished the assignment using a map method inside a reduce method.

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
 def sub_strings(string, dictionary)
+  string = string.delete(",.?!'").downcase
+  string = string.split(' ')
   
-  dictionary.reduce(Hash.new(0)) do |result, word|
-      if string.include? word
-        result[word] += 1
-        result
-      else
-        result
+  dictionary.reduce(Hash.new(0)) do |newhash, word|
+    string.map do |s|
+      if s.include? word
+        newhash[word] += 1
       end
+    end
+    newhash
   end
 end
 
@@ -16,3 +18,4 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low",
   "own","part","partner","sit"]
 
 p sub_strings("below", dictionary)
+p sub_strings("Howdy partner, sit down! How's it going?", dictionary)
